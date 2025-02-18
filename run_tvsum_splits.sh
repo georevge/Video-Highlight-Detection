638cc72f40e345a6940a150208c851bec4e1b31b
georevge <101278572+georevge@users.noreply.github.com>
2025-02-18 16:42:46 +0200

    Update run_tvsum_splits.sh

--- run_tvsum_splits.sh
+++ run_tvsum_splits.sh
@@ -1,1 +1,5 @@
-
+LC_NUMERIC="en_US.UTF-8"
+for i in $(seq 0 4); do
+  python model/main.py --split_index "$i" --n_epochs 300 --batch_size 40 --video_type 'TVSum'
+  python evaluation/compute_fscores.py --path ".../SumMe/results/split$i" --dataset TVSum --eval avg
+done
